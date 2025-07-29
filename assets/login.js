34dac482d2db2ad5f45b14dac0373bd22352f680
Erwan Charef <erwan.charef@gmail.com>
2025-07-29 17:13:56 +0200

    Set up Login

---
+++ assets/login.js
@@ -0,0 +1,35 @@
+function login(event) { 
+    event.preventDefault();
+
+    console.log("Login button clicked");
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        return;
+    }
+    
+    fetchData("users/login", "POST", false, { email, password })
+        .then(response => {
+            //Get the token 
+            const token = response.token;
+            if (token) {    
+                localStorage.setItem("token", token);
+            }
+
+            //return to main page 
+            window.location.href = "index.html";
+        })
+        .catch(error => {
+            console.error("Login failed:", error);
+        });
+}
+
+addEventListener("DOMContentLoaded", () => { 
+    //Login on submit
+    const loginForm = document.getElementById("login-form");
+    if (loginForm) {
+        loginForm.addEventListener("submit", login);
+    }
+})
